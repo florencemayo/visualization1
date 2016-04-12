3e793f344525b581569ad1ea2b994d245d095636
florencemayo <florencemayo11@gmail.com>
2016-04-12 12:00:44 +0200

    data element can now be printed, data can be added to a new file

--- src/javafiles/WriteToJsonFile.java
+++ src/javafiles/WriteToJsonFile.java
@@ -1,6 +1,9 @@
 package javafiles;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -9,7 +12,10 @@ import com.google.gson.JsonParser;
 
 public class WriteToJsonFile {
 	public static void main(String[] args){
-	    //READ FROM A FILE
+	    //INITIALIZE A STRING TO copy the content;
+		String content="{\n";
+		
+		//READ FROM A FILE
 		JsonParser parser = new JsonParser();
 		JsonElement element=null;
 		try {
@@ -26,6 +32,11 @@ public class WriteToJsonFile {
 		if (element.isJsonObject()){
 			JsonObject ct=element.getAsJsonObject();
 			System.out.println(ct.get("elementName").getAsString());
+
+			//concatenate name of the susbsytem
+			content+='"'+"elementName"+'"'+" : "+'"'+ct.get("elementName").getAsString()+'"'+"\n";
+			
+
 			JsonArray contentRelations =ct.getAsJsonArray("contentRelations");
 			for (int i=0;i<contentRelations.size();i++){
 				JsonObject ctr = contentRelations.get(i).getAsJsonObject();
@@ -37,6 +48,10 @@ public class WriteToJsonFile {
 				System.out.println("------------*****************--------------------------");
 				System.out.println(dest.get("elementName").getAsString());	
 				System.out.println("------------*****************--------------------------");
+
+				//concatenate name of the LCs
+
+
 				//PRINT ALL PORTS and the direction of a port
 				JsonArray contentRelationsDest =dest.getAsJsonArray("contentRelations");
 				JsonObject destP=new JsonObject();
@@ -44,21 +59,50 @@ public class WriteToJsonFile {
 					JsonObject ctrP = contentRelationsDest.get(j).getAsJsonObject();
 					destP=(JsonObject) ctrP.get("destination");
 				    if (destP.get("className").getAsString().equals("PORT")){
-				    System.out.println(destP.get("elementName").getAsString());
-				    //System.out.println(destP.get("IODirection").getAsString());
-				    }
-				   				
-					//PRINT ALL DATA ELEM
-					/*JsonArray contentRelationsDestPort=destP.getAsJsonArray("contentRelations");
-					for (int k=0; k<contentRelationsDestPort.size();k++){
+				    System.out.println("PORT NAME :"+destP.get("elementName").getAsString());
+				    
+				    //PRINT ALL DATA ELEM
+					JsonArray contentRelationsDestPort=destP.getAsJsonArray("contentRelations");
+					int k=0;
+					while(k<contentRelationsDestPort.size()){
 						JsonObject ctrpElem =contentRelationsDestPort.get(k).getAsJsonObject();
 						JsonObject destElem=(JsonObject)ctrpElem.get("destination");
-						if (destElem.get("classType").getAsString().equals("DATA-ELEM"))
-							System.out.println(destElem.get("elementName").getAsString());
-					}*/
+						if (destElem.get("className").getAsString().equals("DATA-ELEM"))
+							System.out.println("DATA ELEM :"+destElem.get("elementName").getAsString());
+						    k++;
+					}
+					
+					//PRINT ALL IODIRECTION
+				    JsonObject ctrPIO=destP.get("attributes").getAsJsonObject();
+				    System.out.println("IODIRECTION : "+ctrPIO.get("IODirection").getAsString());
+				    
+					
+				    }
 				 }
 				}
 			}
 		}
+		
+		//WRITE TO A NEW JSON FILE
+		content+="}";
+		try {
+		File file = new File("E://programs/eclipse/workspace/Visualization1/src/jsons/Visibility Control SPA.json");
+		
+		//check if the file exists
+		if (!file.exists()){
+			file.createNewFile();
+		}
+
+		FileWriter fw = new FileWriter (file.getAbsolutePath());
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write(content);
+		bw.close();
+
+		System.out.println("DONE");
+	} catch (IOException e){
+      e.printStackTrace();
+	}
+
+
 	}
 }
